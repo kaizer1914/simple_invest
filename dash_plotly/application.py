ddfc31cc2da1c1b5f8eb90e874d6fe49b118f366
kaizer1914 <kirill.kodryanu@mail.ru>
2021-11-03 21:27:12 +0300

    Dash от Plotly

--- dash_plotly/application.py
+++
@@ -1,4 +0,0 @@
-from dash import Dash
-
-app = Dash('invest', suppress_callback_exceptions=True)
-server = app.server
---
+++ streamlit/main.py
@@ -0,0 +1,9 @@
+import streamlit
+
+from vtb.position_report import PositionReport
+
+position_report = PositionReport()
+upload = streamlit.file_uploader('Upload')
+if upload is not None:
+    data = position_report.save_to_db(upload.getvalue())
+    streamlit.write(data)
