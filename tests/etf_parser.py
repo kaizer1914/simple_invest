129133b6b413e15e8e262bf80e3ee1fe45cc34d9
kaizer1914 <kirill.kodryanu@mail.ru>
2021-11-13 00:34:48 +0300

    create parser of bpif on moex stock

--- tests/etf_parser.py
+++ tests/etf_parser.py
@@ -4,3 +4,74 @@ https://www.moex.com/msn/etf
 и последующего сохранения в БД
 '''
 
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+
+class EtfParser:
+    def __init__(self):
+        url = 'https://www.moex.com/msn/etf'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        table = soup.findAll('table')[1].tbody
+        rows = table.find_all('tr')
+        data = list()
+
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [element.text.strip() for element in cols]
+            data.append([element for element in cols if element])
+        data.pop(0)
+
+        bonds_index = 0
+        cash_index = 0
+        shares_index = 0
+        products_index = 0
+        gold_index = 0
+        mix_assets_index = 0
+
+        for row in data:
+            if row[0] == 'Облигации/Еврооблигации':
+                bonds_index = data.index(row)
+            elif row[0] == 'Денежный рынок':
+                cash_index = data.index(row)
+            elif row[0] == 'Акции':
+                shares_index = data.index(row)
+            elif row[0] == 'Товары':
+                products_index = data.index(row)
+            elif row[0] == 'Золото':
+                gold_index = data.index(row)
+            elif row[0] == 'Смешанные активы':
+                mix_assets_index = data.index(row)
+
+        self.__bonds_list = data[bonds_index + 1:cash_index]
+        self.__cash_list = data[cash_index + 1:shares_index]
+        self.__shares_list = data[shares_index + 1:products_index]
+        self.__gold_list = data[gold_index + 1:mix_assets_index]
+        self.__mix_assets_list = data[mix_assets_index + 1:]
+
+    def get_bonds_df(self):
+        for row in self.__bonds_list:
+            row[1] = row[1].replace('\n', '').replace('\r', '').replace('\t', '')
+            row[2] = row[2].replace('\n', '').replace('\r', '').replace('\t', '')
+            row[3] = row[3].replace('\n', '').replace('\r', '').replace('\t', '')
+
+        df = pandas.DataFrame(self.__bonds_list)
+        df = df.rename({
+            1: 'fund',
+            2: 'provider',
+            3: 'base_asset',
+            4: 'currency',
+            5: 'type',
+            6: 'ticker',
+        }, axis='columns')
+        df = df.drop(0, axis='columns')  # Удаляем номера строк
+        df = df.drop(7, axis='columns')  # Удаляем бесполезную колонку
+        return df
+
+
+if __name__ == '__main__':
+    parser = EtfParser()
+    dataframe = parser.get_bonds_df()
+    print(dataframe['ticker'])
